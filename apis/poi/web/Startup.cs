b9dc368a370bb73ea2fa66fc0bc0f734f31769cb
Manuel Sanchez <manuss20@gmail.com>
2021-05-20 12:56:56 +0200

    Update for fail!

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -47,6 +47,8 @@ namespace poi
             });
         }
 
+        HELLO WORD FOR FAIL!!!
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
