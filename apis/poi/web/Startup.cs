3bb38fe53be66a34c143b387902d0afe6a055fbd
Manuel Sanchez <manuss20@gmail.com>
2021-05-20 12:58:51 +0200

    Fix Problem .Net core

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -47,8 +47,6 @@ namespace poi
             });
         }
 
-        HELLO WORD FOR FAIL!!!
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
